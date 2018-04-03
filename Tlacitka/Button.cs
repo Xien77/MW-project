536ea7aad9cc2e9890e6c44de6f33a6ddd5c950a
fuxapatrik <fuxapatrik@orangedom.orangefactory.cz>
2018-04-03 11:00:24 +0200

    Add project files.

---
+++ Tlacitka/Button.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tlacitka
+{
+    public class Button
+    {
+        
+        public new Vector2 Position;
+        public Texture2D Texture;
+        public int Width;
+        public int Heigth;
+
+
+
+        public Button(int positionX, int positionY, int heigth, int width)
+        {
+            Width = width;
+            Heigth = heigth;
+
+            Position = new Vector2(positionX, positionY);           
+            Texture = new Texture2D(Game1.instance.GraphicsDevice, width, heigth);         
+            
+            Color[] square = new Color[width * heigth];
+            for (int i = 0; i < square.Length; i++) square[i] = Color.DarkRed;
+            Texture.SetData(square);
+
+        }
+
+
+        public bool MouseTracking()
+        {
+            
+            if (Mouse.GetState().X >= Position.X || Mouse.GetState().X <= Width)
+            { if (Mouse.GetState().Y >= Position.Y || Mouse.GetState().Y <= Heigth)
+                {
+                    return true;
+
+                }
+
+            }              
+        }
+
+        public bool IsClicked()
+        {
+            if (MouseTracking() = true | Mouse.().LeftButton)
+        }
+
+    }
+}
---
+++ Tlacitka/Game1.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Tlacitka
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Button button;
+        Texture2D cursor;
+        Vector2 cursorPosition;
+        public static Game1 instance;
+
+        public Game1()
+        {
+            instance = this;
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            
+
+            button = new Button(100, 100, 40, 100);
+
+            cursorPosition = new Vector2(20, 20);
+            cursor = new Texture2D(GraphicsDevice, 20, 20);
+            Color[] square2 = new Color[20 * 20];
+            for (int ii = 0; ii < square2.Length; ii++) square2[ii] = Color.Wheat;
+            cursor.SetData(square2);
+
+
+
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            cursorPosition = new Vector2(Mouse.GetState().X, Mouse.GetState().Y);
+
+            
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+
+            spriteBatch.Draw(button.Texture, button.Position);
+            spriteBatch.Draw(cursor, cursorPosition, origin: new Vector2(cursor.Width / 2, cursor.Height / 2));
+
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
